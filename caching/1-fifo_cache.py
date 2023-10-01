8b7dbfedc23c5d96fd100923390f8e222e17688f
YourGitHubUsername <YourGitHubEmail>
2023-10-01 18:47:01 +0200

    FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+class FIFOCache that inherits from
+BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def __init__(self):
+        """ Initialize FIFOCache """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Get the first item that was added (FIFO)
+                first_key = next(iter(self.cache_data))
+                print(f"DISCARD: {first_key}")
+                self.cache_data.pop(first_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
