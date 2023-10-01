4b551204e8182e9cd0eaa9dc71931f7c13a81453
YourGitHubUsername <YourGitHubEmail>
2023-10-01 23:20:19 +0200

    Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Calculate the start and end indices for a given page and page_size.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start index and an end index
+    """
+    end: int = page * page_size
+    start: int = end - page_size
+    
+    return (start, end)
