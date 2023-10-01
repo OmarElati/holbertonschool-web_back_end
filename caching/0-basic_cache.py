296b50b3b9c607803915e06ab26d8530278f2db1
YourGitHubUsername <YourGitHubEmail>
2023-10-01 18:42:05 +0200

    Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+class BasicCache that inherits from BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class """
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
