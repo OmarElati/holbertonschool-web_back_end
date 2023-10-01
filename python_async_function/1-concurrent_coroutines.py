6f60e67a23e31841282f334f02facd4bae73baa6
YourGitHubUsername <YourGitHubEmail>
2023-10-01 17:16:33 +0200

    Let's execute multiple coroutines at the same time with async

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,8 @@ import heapq
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds
+    Asynchronous coroutine that waits for a
+    random delay between 0 and max_delay seconds
     (inclusive) and returns it as a float.
 
     param max_delay: The maximum delay in seconds (default is 10).
@@ -20,10 +21,13 @@ async def wait_random(max_delay: int = 10) -> float:
     await asyncio.sleep(delay)
     return delay
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous routine that spawns wait_random n times with the specified max_delay
-    and returns the list of all the delays (float values) in ascending order.
+    Asynchronous routine that spawns
+    wait_random n times with the specified
+    max_delay and returns the list of all
+    the delays (float values) in ascending order.
 
     param n: The number of times to call wait_random.
     param max_delay: The maximum delay in seconds for wait_random.
