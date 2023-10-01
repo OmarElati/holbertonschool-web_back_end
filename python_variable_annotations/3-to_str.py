37172da2df0782f44a033a8aa64583f711d203eb
YourGitHubUsername <YourGitHubEmail>
2023-10-01 16:05:57 +0200

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Converts a floating-point number to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    param n: The input floating-point number.
+    return: The string representation of the input number.
+    """
+    return str(n)
