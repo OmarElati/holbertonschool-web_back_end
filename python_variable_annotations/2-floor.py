c5d760f30d635fef17030a6c0284d3b96bf7363c
YourGitHubUsername <YourGitHubEmail>
2023-10-01 15:47:10 +0200

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+import math
+"""
+Returns the floor of a floating-point number.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    param n: The input floating-point number.
+    return: The floor value as an integer.
+    """
+    return math.floor(n)
