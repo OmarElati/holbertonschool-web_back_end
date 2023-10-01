0012ecc554ee0dd3d221ac99b4241d6761888205
YourGitHubUsername <YourGitHubEmail>
2023-10-01 15:43:31 +0200

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Concatenates two strings and returns the result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    param str1: The first input string.
+    param str2: The second input string.
+    return: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
