2f54bab07c41356f869c91395481df76c0a5a4c4
YourGitHubUsername <YourGitHubEmail>
2023-10-01 16:20:38 +0200

    Complex types - functions*

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Returns a function that multiplies a float by the given multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    param multiplier: The multiplier to be used in the returned function.
+    return: A function that takes a float and returns the result of multiplying it by the multiplier.
+    """
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+
+    return multiplier_function
