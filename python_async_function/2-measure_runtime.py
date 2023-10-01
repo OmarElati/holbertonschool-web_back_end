97071b9ac102784c7f5d5ecfa73fe99c986d7d54
YourGitHubUsername <YourGitHubEmail>
2023-10-01 17:50:02 +0200

    Measure the runtime

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-Measures the total execution time for wait_n(n, max_delay) and returns total_time / n.
+Measures the total execution time for
+wait_n(n, max_delay) and returns total_time / n.
 """
-from typing import List
 import time
 import asyncio
 
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines')
 
 
 async def measure_time(n: int, max_delay: int) -> float:
@@ -17,11 +17,6 @@ async def measure_time(n: int, max_delay: int) -> float:
     return: The average execution time per call.
     """
     start_time = time.time()
-
-    await asyncio.run(wait_n(n, max_delay))
-
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
-
-    total_time = end_time - start_time
-
-    return total_time / n
+    return (end_time - start_time) / n
