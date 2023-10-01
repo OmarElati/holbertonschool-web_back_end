53c5f586aa793229d7a527fc1eb5235daa268258
YourGitHubUsername <YourGitHubEmail>
2023-10-01 18:05:09 +0200

    Tasks

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -9,7 +9,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     param max_delay: The maximum delay in seconds for wait_random.
     return: An asyncio.Task representing the execution of wait_random.
