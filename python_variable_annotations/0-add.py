54a6c5c28839b8567ee787f48c1d493103b6e960
YourGitHubUsername <YourGitHubEmail>
2023-10-01 15:40:33 +0200

    Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
 Adds two floating-point numbers and returns their sum.
-
-param a: The first floating-point number.
-param b: The second floating-point number.
-return: The sum of `a` and `b` as a floating-point number.
 """
 
 
 def add(a: float, b: float) -> float:
+    """
+    param a: The first floating-point number.
+    param b: The second floating-point number.
+    return: The sum of `a` and `b` as a floating-point number.
+    """
     return a + b
