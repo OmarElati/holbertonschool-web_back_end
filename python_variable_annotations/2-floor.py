707188a76aac078370f1b589790b3ee554ad5ffd
YourGitHubUsername <YourGitHubEmail>
2023-10-01 15:50:44 +0200

    Basic annotations - floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import math
 """
-Returns the floor of a floating-point number.
+This module defines a function for calculating the floor of a floating-point number.
 """
 
 
