b545f98367791e6bc00d7395696809d66b5bfa8a
YourGitHubUsername <YourGitHubEmail>
2023-10-01 22:15:50 +0200

    MRU Caching

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -12,23 +12,22 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ Initialize MRUCache """
         super().__init__()
+        self.lst = []
 
     def put(self, key, item):
         """ Add an item to the cache """
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key = next(reversed(self.cache_data))
-                print(f"DISCARD: {mru_key}")
-                del self.cache_data[mru_key]
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        self.lst.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last = self.lst.pop(-2)
+            print('DISCARD:', last)
+            del self.cache_data[last]
 
     def get(self, key):
         """ Retrieve an item from the cache """
-        if key is None or key not in self.cache_data:
-            return None
-
-        # Move the accessed key to the end of the cache_data dictionary
-        item = self.cache_data.pop(key)
-        self.cache_data[key] = item
-
-        return item
+        if key in self.cache_data:
+            self.lst.append(key)
+        if key in self.cache_data:
+            return self.cache_data[key]
