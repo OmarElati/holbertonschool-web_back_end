2ca9a1c86fbf4f74213f10a9f48fd0670e72e5b3
YourGitHubUsername <YourGitHubEmail>
2023-10-09 00:56:28 +0200

    test

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -2,85 +2,81 @@
 """
 Route module for the API
 """
-from binascii import rledecode_hqx
-from logging import basicConfig
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
+from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from auth.session_auth import SessionAuth
-from api.v1.auth.basic_auth import BasicAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth_type = os.getenv('AUTH_TYPE')
 
-
-if os.getenv("AUTH_TYPE") == "basic_auth":
-    auth = BasicAuth()
-if os.getenv("AUTH_TYPE") == "session_auth":
-    auth = SessionAuth()
-else:
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """
-    forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
+elif auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(401)
-def request_unauthorized(error) -> str:
-    """
-    unauthorized handler
+def unauthorized(error):
+    """ Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error):
+    """
+    Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """
-    Not found handler
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.before_request
-def before_req() -> None:
+def before_request_func():
     """
-    Not found handler
+    Executed before each request that is handled by a function of the
+    Blueprint.
     """
+    excluded_paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
     if auth is None:
         return
-    routes_list = [
-        "/api/v1/status/",
-        "/api/v1/unauthorized/",
-        "/api/v1/forbidden/",
-        "/api/v1/auth_session/login/",
-    ]
-
-    if auth.require_auth(request.path, routes_list) is False:
+    if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if (
-        auth.authorization_header(request) is None
-        and auth.session_cookie(request) is None
-    ):
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
-
-    request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-    
