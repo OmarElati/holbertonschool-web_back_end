b9595fc119081361e1f55548844ec78ea2531f6e
YourGitHubUsername <YourGitHubEmail>
2023-10-08 23:57:34 +0200

    Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -13,6 +13,6 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
-    Use bcrypt to validate that the provided password matches the hashed password. 
+    Use bcrypt to validate that provided password matches hashed password. 
     """
     return bcrypt.checkpw(password.encode(), hashed_password)
