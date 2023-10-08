cf8f02080f7f101d0cf41c163885a4356f26d239
YourGitHubUsername <YourGitHubEmail>
2023-10-08 22:53:36 +0200

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ function called filter_datum that returns the log message obfuscated """
+    """ function called filter_datum returns the log message obfuscated """
     lst = message.split(separator)
 
     for f in fields:
