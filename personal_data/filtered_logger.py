7d3454c95cbf3a5f4e236e27609618e86fc188e1
YourGitHubUsername <YourGitHubEmail>
2023-10-08 23:46:30 +0200

    Connect to secure database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -68,7 +68,9 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ Get database credentials from environment variables with default values """
+    """
+    Get database credentials from environment variables with default values
+    """
     user = os.getenv("PERSONAL_DATA_DB_USERNAME", 'root')
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", '')
     host = os.getenv("PERSONAL_DATA_DB_HOST", 'localhost')
