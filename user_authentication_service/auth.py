5a20f0429df91597e69aa63005df02922f8c934b
YourGitHubUsername <YourGitHubEmail>
2023-10-15 14:57:48 +0200

    add auth

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,6 +5,8 @@ Find user by session ID, Destroy session, Generate reset password token,
 Update password
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +14,21 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        existing_user = self._db.find_user_by(email=email)
+        if existing_user:
+            raise ValueError(f"User {email} already exists")
+
+        hashed_password = _hash_password(password)
+
+        new_user = self._db.add_user(email, hashed_password)
+        return new_user
