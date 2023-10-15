117aa65189338b19febc8d742cc6dff579dddcf9
YourGitHubUsername <YourGitHubEmail>
2023-10-15 15:24:09 +0200

    Basic Flask app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,25 +3,41 @@ from flask import Flask, jsonify, request
 from auth import Auth
 
 app = Flask(__name__)
+app.debug = True
+
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/', methods=['GET'])
 def welcome():
-    """ Basic Flask app, return a JSON """
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def users():
-    """ Register user """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 200
-    except Exception:
-        return jsonify({"message": "email already registered"}), 400
+def users() -> str:
+    """ form-data uses request.form, body JSON uses request.get_json() """
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+
+        email = request.form.get("email")
+        pswd = request.form.get("password")
+
+        try:
+            new_user = AUTH.register_user(email, pswd)
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
