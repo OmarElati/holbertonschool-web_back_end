efbee8b5b6409b0a5ab2d9f77dd53fe4308c6048
YourGitHubUsername <YourGitHubEmail>
2023-10-15 15:13:09 +0200

    Basic Flask app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
