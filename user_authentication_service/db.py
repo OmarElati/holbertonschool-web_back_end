366dbca6f276c202928cd41b34d5945b366aeb45
YourGitHubUsername <YourGitHubEmail>
2023-10-15 14:32:18 +0200

    create user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -30,8 +30,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database and return the User object"""
+    def add_user(self, email, hashed_password):
+        """
+        Add a new user to the database and return the User object
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
