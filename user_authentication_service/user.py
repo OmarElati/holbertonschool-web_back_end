b947b814f4f2f1229c24bb1f8412d86575344227
YourGitHubUsername <YourGitHubEmail>
2023-10-15 14:12:21 +0200

    User model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+SQLAlchemy model - User ORM
+"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    class to create database table for users
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
