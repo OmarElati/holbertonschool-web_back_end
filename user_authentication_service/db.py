19be222c8e2f7e4a2363d34baadecd9fed0d0629
YourGitHubUsername <YourGitHubEmail>
2023-10-15 14:29:02 +0200

    create user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,8 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
-from user import Base
+
+from user import Base, User
 
 
 class DB:
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a new user to the database and return the User object
         """
