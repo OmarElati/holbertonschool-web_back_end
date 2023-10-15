4e4cf65896c7d7284c87df3d289227fd622cea7b
YourGitHubUsername <YourGitHubEmail>
2023-10-15 15:47:11 +0200

    Update

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -59,6 +59,6 @@ class Auth:
         except NoResultFound:
             return None
 
-        session_id = str(uuid.uuid4())
+        session_id = _generate_uuid()
         self._db.update_user(found_user.id, session_id=session_id)
         return session_id
