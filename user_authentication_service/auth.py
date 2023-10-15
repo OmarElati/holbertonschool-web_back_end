5b4bfa9d0f0ffac22288078c894ff38b8865c6a2
YourGitHubUsername <YourGitHubEmail>
2023-10-15 15:27:34 +0200

    file Update

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -36,7 +36,7 @@ class Auth:
         new_user = self._db.add_user(email, hashed_password)
         return new_user
 
-    def valid_login(self, email, password):
+    def valid_login(self, email: str, password: str) -> bool:
         try:
             user = self._db.find_user_by(email=email)
             if user:
