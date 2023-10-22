98fb66e3d8ce582fec3bd8ea773123eac06c1815
OmarElati <5796@holbertonstudents.com>
2023-10-22 12:37:50 +0200

    test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,56 +1,21 @@
-#!/usr/bin/env python3
-"""Test Client File"""
-
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
-from unittest.mock import patch, PropertyMock
-
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Test Github Org Client Class"""
 
     @parameterized.expand([
-        ("google"),
-        ("abc")
+        ("google",),
+        ("abc",)
     ])
-    @patch('client.get_json')
-    def test_org(self, test_org, mock_json):
-        """Test Org Method"""
-        client = GithubOrgClient(test_org)
-        client.org()
-        mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{test_org}"
-        )
+    @patch('client.get_json', return_value={"org_name": "dummy_org"})
+    def test_org(self, org_name, mock_get_json):
+        client = GithubOrgClient(org_name)
+        result = client.org()
 
-    def test_public_repos_url(self):
-        """Test Public Repos Url Method"""
-        with patch.object(
-            GithubOrgClient,
-            "org",
-            new_callable=PropertyMock,
-            return_value={'repos_url': 'holberton'}
-        ) as mock_get:
-            test_json = {"repos_url": 'holberton'}
-            test_client = GithubOrgClient(test_json.get("repos_url"))
-            test_return = test_client._public_repos_url
-            mock_get.assert_called_once
-            self.assertEqual(
-                test_return,
-                mock_get.return_value.get("repos_url")
-            )
+        mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+        self.assertEqual(result, {"org_name": "dummy_org"})
 
-    @patch("client.get_json", return_value=[{"name": "holberton"}])
-    def test_public_repos(self, mock_get):
-        """Test public repos Method"""
-        with patch.object(
-            GithubOrgClient,
-            "_public_repos_url",
-            new_callable=PropertyMock,
-            return_value="https://api.github.com/"
-        ) as mock_pub:
-            test_client = GithubOrgClient("holberton")
-            test_return = test_client.public_repos()
-            self.assertEqual(test_return, ["holberton"])
-            mock_get.assert_called_once
-            mock_pub.assert_called_once
+if __name__ == '__main__':
+    unittest.main()
