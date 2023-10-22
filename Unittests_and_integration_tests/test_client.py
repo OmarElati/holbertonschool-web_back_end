84466af48448a850925938ebd82cef4eac170949
OmarElati <5796@holbertonstudents.com>
2023-10-22 12:03:05 +0200

    Mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,58 +1,65 @@
 #!/usr/bin/env python3
-"""A github org client
-"""
-from typing import (
-    List,
-    Dict,
+""" Now we will test the clients.py file """
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+from requests import Response
+from unittest.mock import Mock, PropertyMock, patch
+import unittest
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ New class for testing GithubOrgClient """
+
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    def test_org(self, org_name):
+        """ Test func for .org """
+        with patch('client.GithubOrgClient.org') as m_org:
+            client = GithubOrgClient(org_name)
+            self.assertEqual(client.org.return_value, m_org.return_value)
+
+    def test_public_repos_url(self):
+        """ Another test """
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock)\
+                as m_org:
+            m_org.return_value = {"repos_url": True}
+            client = GithubOrgClient("org_name")
+            self.assertEqual(client._public_repos_url, True)
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """ Method to test client.GithubOrgClient.public_repos """
+        with patch('client.GithubOrgClient.public_repos',
+                   new_callable=PropertyMock) as m_prop:
+            mock_json.return_value = {'repos_url': 'www.randomgenerator.com'}
+            client = GithubOrgClient('randomgenerator')
+            m_prop.return_value = client.org.get('repos_url')
+            self.assertEqual(client.public_repos, 'www.randomgenerator.com')
+            m_prop.assert_called_once()
+            mock_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ Method in order to unit-test GithubOrgClient.has_license """
+        The_test = GithubOrgClient('org_name')
+        self.assertEqual(The_test.has_license(repo, license_key), expected)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    TEST_PAYLOAD
 )
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ Integration test; inmplementing two methods """
 
-from utils import (
-    get_json,
-    access_nested_map,
-    memoize,
-)
+    def setUpClass():
+        """ Setup Class; mock requests.get to return example payloads """
 
-
-class GithubOrgClient:
-    """A Githib org client
-    """
-    ORG_URL = "https://api.github.com/orgs/{org}"
-
-    def __init__(self, org_name: str) -> None:
-        """Init method of GithubOrgClient"""
-        self._org_name = org_name
-
-    @memoize
-    def org(self) -> Dict:
-        """Memoize org"""
-        return get_json(self.ORG_URL.format(org=self._org_name))
-
-    @property
-    def _public_repos_url(self) -> str:
-        """Public repos URL"""
-        return self.org["repos_url"]
-
-    @memoize
-    def repos_payload(self) -> Dict:
-        """Memoize repos payload"""
-        return get_json(self._public_repos_url)
-
-    def public_repos(self, license: str = None) -> List[str]:
-        """Public repos"""
-        json_payload = self.repos_payload
-        public_repos = [
-            repo["name"] for repo in json_payload
-            if license is None or self.has_license(repo, license)
-        ]
-
-        return public_repos
-
-    @staticmethod
-    def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
-        """Static: has_license"""
-        assert license_key is not None, "license_key cannot be None"
-        try:
-            has_license = access_nested_map(repo, ("license", "key")) == license_key
-        except KeyError:
-            return False
-        return has_license
+    def tearDownClass():
+        """ Teardown Class to stop the patcher """
