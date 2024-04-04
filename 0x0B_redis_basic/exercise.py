b14c094a0a13c3f437771b44ebf41f7593a094ec
OmarElati <5796@holbertonstudents.com>
2024-04-04 04:18:47 +0200

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -9,7 +9,7 @@ Classes:
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -35,3 +35,46 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis based on the given key.
+
+        Args:
+            key (str): The key associated with the data to be retrieved.
+            fn (Callable, optional): A callable function used to convert the retrieved data.
+                Defaults to None.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data.
+        """
+        if self._redis.exists(key):
+            data = self._redis.get(key)
+            if fn:
+                return fn(data)
+            return data
+        return None
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve string data from Redis based on the given key.
+
+        Args:
+            key (str): The key associated with the string data to be retrieved.
+
+        Returns:
+            Union[str, None]: The retrieved string data.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieve integer data from Redis based on the given key.
+
+        Args:
+            key (str): The key associated with the integer data to be retrieved.
+
+        Returns:
+            Union[int, None]: The retrieved integer data.
+        """
+        return self.get(key, fn=lambda d: int(d))
