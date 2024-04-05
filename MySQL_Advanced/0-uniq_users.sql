e191b68a17ba2ec4f621876cb294a3b6d1ef7436
OmarElati <5796@holbertonstudents.com>
2024-04-05 07:12:49 +0200

    We are all unique!

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- Create a table named 'users' if it doesn't already exist
+-- with attributes: id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
