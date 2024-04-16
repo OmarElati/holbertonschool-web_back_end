b1f7a461e557947c83141a9becf212a5907a5272
OmarElati <5796@holbertonstudents.com>
2024-04-16 16:15:38 +0200

    buy buy buy

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Trigger that decreases the quantity of an item
+-- after adding a new order
+CREATE TRIGGER trg_decrease
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
