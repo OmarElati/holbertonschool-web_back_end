17a5efbef014d44a6870b41c166b439e3b9cbc39
OmarElati <5796@holbertonstudents.com>
2024-04-17 18:13:03 +0200

    Optimize search and score by creating index on first letter of name and score

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
--- script that creates an index idx_name_first_score.
--- on the table names and the first letter of name and the score.
+-- Script That Creates An Index idx_name_first_score.
+-- On The Table names And The First Letter Of name And The score.
 CREATE INDEX idx_name_first_score ON names (name(1), score);
