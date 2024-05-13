90440cc89cae367ac5aabfc832c4d9cc03f5fe9b
OmarElati <5796@holbertonstudents.com>
2024-05-14 01:17:47 +0200

    List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,7 @@
 #!/user/bin/env python3  
-" List All Docuements In A Collection "
+""" List All Docuements In A Collection """
 
 
 def list_all(mongo_collection):
-    " List All Docuements In A Collection "
+    """ List All Docuements In A Collection """
     return mongo_collection.find()
