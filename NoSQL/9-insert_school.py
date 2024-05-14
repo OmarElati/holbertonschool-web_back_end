4e3d792980446cde043b0e825d244dc519bb0e4e
OmarElati <5796@holbertonstudents.com>
2024-05-15 00:37:28 +0200

    Implement function to insert a new document into a MongoDB collection based on provided data

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Inserts A New Document In A Collection Based On kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs:
+        param mongo_collection: pymongo collection object
+        param kwargs: keyword arguments representing the data to insert
+        return: the new _id
+    """
+    new_school_id = mongo_collection.insert_one(kwargs).inserted_id
+    return str(new_school_id)
