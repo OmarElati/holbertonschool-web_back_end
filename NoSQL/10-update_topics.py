8d1a4d4e59a14af5ed418996c179da84741a8492
OmarElati <5796@holbertonstudents.com>
2024-05-15 00:50:10 +0200

    Implement function to update school topics based on school name in MongoDB collection

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Changes All Topics Of A School Document Based On The Name """
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes All Topics Of A School Document Based On The Name
+        param mongo_collection: pymongo collection object
+        param name: school name to update
+        param topics: list of topics approached in the school
+    """
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
