3c26afc06e572eea11ee7c77627d82081aef138e
OmarElati <5796@holbertonstudents.com>
2024-08-06 18:11:39 +0200

    A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
@@ -9,22 +13,10 @@ export default class HolbertonCourse {
     if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-
-    this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get name() {
     return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
   }
 
   set name(value) {
@@ -34,11 +26,19 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = value;
+  }
+
+  get students() {
+    return this._students;
   }
 
   set students(value) {
