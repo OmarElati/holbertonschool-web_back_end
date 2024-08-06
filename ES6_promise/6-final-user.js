24f991f32d8101b2dd28a875c5e72830d848c04d
OmarElati <5796@holbertonstudents.com>
2024-08-06 14:30:11 +0200

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,22 +2,33 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-    try {
-        const userPromise = signUpUser(firstName, lastName);
-        const photoPromise = uploadPhoto(fileName);
+    
+  const user = {
+      status: 'pending',
+      value: '',
+  };
+  const photo = {
+      status: 'pending',
+      value: '',
+  };
 
-        await Promise.all([userPromise, photoPromise]);
-
-        return [
-            { status: 'fulfilled', value: await userPromise },
-            { status: 'fulfilled', value: await photoPromise }
-        ];
-    } catch (error) {
-        return [
-            { status: 'rejected', value: error },
-            { status: 'rejected', value: error }
-        ];
-    }
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    user.status = 'fulfilled';
+    user.value = signup;
+  } catch (error) {
+    user.status = 'rejected';
+    user.value = error.toString();
+  }
+  try {
+    const upload = await uploadPhoto(fileName);
+    photo.status = 'fulfilled';
+    photo.value = upload;
+  } catch (error) {
+    photo.status = 'rejected';
+    photo.value = error.toString();
+  }
+  return [user, photo];
 }
 
 export default handleProfileSignup;
