592f8c5bd256c5c22b50df58daf134607fbdd31e
OmarElati <5796@holbertonstudents.com>
2024-08-06 14:36:06 +0200

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-    
   const user = {
-      status: 'pending',
-      value: '',
+    status: 'pending',
+    value: '',
   };
   const photo = {
-      status: 'pending',
-      value: '',
+    status: 'pending',
+    value: '',
   };
 
   try {
