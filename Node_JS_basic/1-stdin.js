fbbf9d0842528a861727988a175ed3734e48f1ba
OmarElati <5796@holbertonstudents.com>
2024-08-07 02:33:30 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf-8');
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  process.exit();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
