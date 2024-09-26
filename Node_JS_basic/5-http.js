03e3104a1838ce7de258e30ab9b566d70e4df16d
OmarElati <5796@holbertonstudents.com>
2024-09-26 15:40:59 +0100

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,24 +1,20 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const { url } = req;
-  const message = async () => {
-    await countStudents(process.argv[2]);
-  };
-  if (url === '/') res.end('Hello Holberton School!');
-  else if (url === '/students') {
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
     try {
-      const response = `This is the list of our students\n${message}`;
-      res.end(response);
-    } catch (err) {
-      res.end(`${err.message}\n`);
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
     }
   }
+  res.end();
 });
-
 app.listen(1245);
-
 module.exports = app;
